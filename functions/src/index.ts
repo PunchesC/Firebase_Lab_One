323bfa9e7fc5dfb59cd9d770e972484e4672bc26
Curtis <c.b.punches@gmail.com>
2021-06-03 10:40:14 -0400

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+// import * as functions from "firebase-functions";
+export { default as shout_out } from "./routes/route";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ functions/src/routes/route.ts
@@ -0,0 +1,46 @@
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import ShoutOuts from '../model/shoutOuts'
+
+import { getClient } from '../db';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+// res.json({ message: "Banana!" });
+// });
+
+
+app.get("/", async (req, res) => {
+  try {
+    const client = await getClient();
+    const results = await client.db().collection<ShoutOuts>('shoutOuts').find().toArray();
+    res.json(results); // send JSON results
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+
+app.post("/", async (req, res) => {
+  const shoutOut = req.body as ShoutOuts;
+  try {
+    const client = await getClient();
+    const result = await client.db().collection<ShoutOuts>('shoutOuts').insertOne(shoutOut);
+    shoutOut._id = result.insertedId;
+    res.status(201).json(shoutOut);
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+  
+
+export default functions.https.onRequest(app);
+
