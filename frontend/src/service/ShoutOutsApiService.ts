76b36101d4d3aa692b2d97f1df95b4da87560935
Curtis <c.b.punches@gmail.com>
2021-09-08 12:25:37 -0400

    fixing

--- frontend/src/service/ShoutOutsApiService.ts
+++ frontend/src/service/ShoutOutsApiService.ts
@@ -22,7 +22,12 @@ export function deleteShoutOut(shoutOutId: string):Promise<void> {
 
 export function readShoutOutsForTo(to: string):Promise<ShoutOuts[]> {
   return axios.get(baseUrl, {
-    params: { to: to }
+    params: { to : to } 
   }).then(res => res.data);
 }
 
+export function readShoutOutsForToAndFrom(name: string):Promise<ShoutOuts[]>{
+  return axios.get(baseUrl, {
+    params: { name : name } 
+  }).then(res => res.data);
+}
